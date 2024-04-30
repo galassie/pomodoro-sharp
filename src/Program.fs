12916db2577d3addf768bd249f54aca4fd4fa3af
Enrico Galassi <enrico.galassi.88@gmail.com>
2024-05-01 00:12:46 +0200

    Simple prompt

--- src/Program.fs
+++ src/Program.fs
@@ -1,2 +1,20 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open FsSpectre
+open Spectre.Console
+
+let focusTime =
+    AnsiConsole.Prompt
+    <| selectionPrompt {
+        title "[lime]Focus time[/]"
+        choices [| "25 minutes"; "30 minutes"; "45 minutes"; "1 hour" |]
+    }
+
+AnsiConsole.MarkupLine($"You have [lime]{focusTime}[/] of focus time!")
+
+let breakTime =
+    AnsiConsole.Prompt
+    <| selectionPrompt {
+        title "[aqua]Break time[/]"
+        choices [| "5 minutes"; "10 minutes"; "15 minutes"; "20 minutes" |]
+    }
+
+AnsiConsole.MarkupLine($"You have [aqua]{breakTime}[/] of break time!")
