7d7aae3756a3a11b27c874c336c0789517c6f165
Enrico Galassi <enrico.galassi.88@gmail.com>
2024-05-01 12:58:15 +0200

    Displaying progress bar

--- Program.fs
+++ Program.fs
@@ -1,20 +1,44 @@
-﻿open FsSpectre
+﻿open System.Threading.Tasks
+open FsSpectre
 open Spectre.Console
+
+let focusColor = Color.Lime.ToMarkup()
+let breakColor = Color.Aqua.ToMarkup()
 
 let focusTime =
     AnsiConsole.Prompt
     <| selectionPrompt {
-        title "[lime]Focus time[/]"
+        title $"[{focusColor}]Focus time[/]"
         choices [| "25 minutes"; "30 minutes"; "45 minutes"; "1 hour" |]
     }
 
-AnsiConsole.MarkupLine($"You have [lime]{focusTime}[/] of focus time!")
+AnsiConsole.MarkupLine($"You have [{focusColor}]{focusTime}[/] of focus time!")
 
 let breakTime =
     AnsiConsole.Prompt
     <| selectionPrompt {
-        title "[aqua]Break time[/]"
+        title $"[{breakColor}]Break time[/]"
         choices [| "5 minutes"; "10 minutes"; "15 minutes"; "20 minutes" |]
     }
 
-AnsiConsole.MarkupLine($"You have [aqua]{breakTime}[/] of break time!")
+AnsiConsole.MarkupLine($"You have [{breakColor}]{breakTime}[/] of break time!")
+
+let progress = AnsiConsole.Progress()
+
+let result =
+    progress.StartAsync(fun (ctx) ->
+        task {
+            let task1 = ctx.AddTask($"[{focusColor}]Focus[/]")
+            let task2 = ctx.AddTask($"[{breakColor}]Break[/]")
+
+            while (not ctx.IsFinished) do
+                do! Task.Delay(250)
+                task1.Increment(1.5)
+                task2.Increment(0.5)
+
+            return 0
+        })
+
+Task.WaitAll(result)
+
+AnsiConsole.WriteLine("Finished!")
